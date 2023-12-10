a6b1e441edb6081e53bdd9cd7c1a11d73568a6a1
Your Nameemail <shareef.99997@gmail.com>
2023-12-10 11:34:43 +0300

    finished code

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,14 +5,17 @@ import 'package:toodoo/model.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+  
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
+  
 }
 
 class _HomeScreenState extends State<HomeScreen> {
 
   DBHelper? dbHelper;
+  
   late Future<List<TodoModel>> dataList;
 
   @override
@@ -29,7 +32,6 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(),
       appBar: AppBar(
         title: Row(
           children: [
@@ -74,16 +76,92 @@ class _HomeScreenState extends State<HomeScreen> {
                       style: TextStyle(
                         fontSize: 25,
                         fontWeight: FontWeight.bold,
-                        color: Colors.white
+                        color: const Color.fromARGB(255, 3, 3, 3)
                       ),
                     ),
                   );
                 }else{
-                  return Container();
-                  // return ListView.builder(
-                  //   shrinkWrap: true,
-                  //   itemCount: snapshot,
-                  //   );
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: snapshot.data?.length,
+                    itemBuilder: (context, index) {
+                      int todoId = snapshot.data![index].id!.toInt();
+                      String todoTitle = snapshot.data![index].title.toString();
+                      String todoDesc = snapshot.data![index].desc.toString();
+                      String todoDT = snapshot.data![index].dateandtime.toString();
+                      return Dismissible(
+                        key: ValueKey<int>(todoId),
+                        direction:DismissDirection.endToStart ,
+                        background: Container(
+                          color: Colors.redAccent,
+                          child: Icon(Icons.delete_forever, color: Colors.white),
+                        ),
+                        onDismissed: (DismissDirection direction){
+                          setState(() {
+                            dbHelper!.delete(todoId);
+                            dataList =dbHelper!.getDataList();
+                            snapshot.data!.remove(snapshot.data![index]);
+                          });
+                        },
+                        child: Container(
+                          margin: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 244, 156, 73),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black,
+                                blurRadius: 4,
+                                spreadRadius: 1,
+                              )
+                            ]
+                          ),
+                          child: Column(
+                            children: [
+                              ListTile(
+                                contentPadding: EdgeInsets.all(10),
+                                title: Padding(
+                                  padding: EdgeInsets.only(bottom: 10),
+                                  child: Text(todoTitle,
+                                  style: TextStyle(fontSize: 19),
+                                  ),
+                                ),
+                                subtitle: Text(
+                                  todoDesc,
+                                  style: TextStyle(fontSize: 19),
+                                ),
+                              ),
+                              Divider(
+                                color: Colors.black,
+                                thickness: 0.8,
+                              ),
+                              Padding(
+                                padding: EdgeInsets.symmetric(vertical: 3,horizontal: 10),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      todoDT,
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w500,
+                                        fontStyle: FontStyle.italic,
+                                      ),
+                                    ),
+                                    InkWell(
+                                      onTap: (){
+                                        Navigator.push(context, MaterialPageRoute(builder: (context)=>AddUpdateTask(todoId:todoId ,todoTitle:todoTitle ,todoDesc:todoDesc,todoDT: todoDT,update:true )));
+                                      },
+                                      child: Icon(Icons.edit_note,size: 28,color: Colors.green,),
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ]
+                            ),
+                        )
+                      );
+                    },
+                  );
                 }
               },
             )
